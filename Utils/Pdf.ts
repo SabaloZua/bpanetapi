01503c2fdf003dd21fe7f3a79d9013f51c1e3d6c
Sabalo Zua <sabalozua@outlook.pt>
2025-05-21 09:10:58 +0000

    refactor: implique a função de extração removendo o wrapper de promessa desnecessário e melhorando o tratamento de erros

--- Utils/Pdf.ts
+++ Utils/Pdf.ts
@@ -88,66 +88,57 @@ export const comprovativo = async (dados: dadosComprovativo): Promise<Buffer> =>
 
 // função para gerar extrato
 export const extrato = async (dados: dadosExtrato): Promise<Buffer> => {
-    return new Promise(async (resolve, reject) => {
-        try {
+    try {
 
-            const executablePath = await chromium.executablePath("https://github.com/Sparticuz/chromium/releases/download/v133.0.0/chromium-v133.0.0-pack.tar");
-            const browser = await puppeteer.launch({
-                args: chromium.args,
-                defaultViewport: chromium.defaultViewport,
-                executablePath,
-                headless: true,
-            });
+        const executablePath = await chromium.executablePath("https://github.com/Sparticuz/chromium/releases/download/v133.0.0/chromium-v133.0.0-pack.tar");;
+        const browser = await puppeteer.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath,
+            headless: chromium.headless,
+        });
 
-            const page = await browser.newPage();
-            // caminho onde esta o formato do pdf a ser gerado
-            const filePath1 = path.join(__dirname, "../", "Views", "extrato.ejs");
-            // caminho onde esta  do pdf  gerado
-            const filePath2 = path.join(__dirname, "../", "extrato.pdf");
+        const page = await browser.newPage();
 
-            // formatando os valores para moeda
-            const trasacoesFormatadas = dados.trasacoes.map(item => ({
-                ...item,
-                t_descricao: item.t_descricao ? item.t_descricao.split(" ").slice(0, 5).join(" ") : null,
-                t_credito: item.t_credito ? formatarmoeda(parseInt(item.t_credito)) : null,
-                t_debito: item.t_debito ? formatarmoeda(parseInt(item.t_debito)) : null
-            }));
-            // geração do pdf
-            await ejs.renderFile(filePath1, {
-                trasacoes: trasacoesFormatadas,
-                nomeclient: dados.nomeclient,
-                iban: dados.iban,
-                numeroconta: dados.numeroconta,
-                dataInicio: dados.datainicio,
-                dataFim: dados.datafim,
-                saldoinicial: formatarmoeda(dados.saldoinicial)
+        const filePath1 = path.join(__dirname, "../", "Views", "extrato.ejs");
+
+        const trasacoesFormatadas = dados.trasacoes.map(item => ({
+            ...item,
+            t_descricao: item.t_descricao ? item.t_descricao.split(" ").slice(0, 5).join(" ") : null,
+            t_credito: item.t_credito ? formatarmoeda(parseInt(item.t_credito)) : null,
+            t_debito: item.t_debito ? formatarmoeda(parseInt(item.t_debito)) : null
+        }));
+
+        const html = await ejs.renderFile(filePath1, {
+            trasacoes: trasacoesFormatadas,
+            nomeclient: dados.nomeclient,
+            iban: dados.iban,
+            numeroconta: dados.numeroconta,
+            dataInicio: dados.datainicio,
+            dataFim: dados.datafim,
+            saldoinicial: formatarmoeda(dados.saldoinicial)
+        });
+
+        await page.setContent(html, { waitUntil: 'networkidle0' });
+
+        const pdfBuffer = await page.pdf({
+            printBackground: true,
+            displayHeaderFooter: true,
+            headerTemplate: `<div></div>`,
+            footerTemplate: `<div style="width:100%; text-align:right; font-size:8px; margin-right:20px;"><span class="pageNumber"></span> / <span class="totalPages"></span></div>`,
+            width: '210mm',
+            height: '297mm',
+            margin: {
+                top: "5px",
+                bottom: "20px",
+                left: "20px",
+                right: "20px"
             },
-                async (err, html) => {
-                    // Gera o PDF
-                    await page.setContent(html, { waitUntil: 'networkidle0' });
-                    const pdfBuffer = await page.pdf({
-                        printBackground: true,
-                        displayHeaderFooter: true,
-                        headerTemplate: `<div></div>`,
-                        footerTemplate: `<div style="width:100%; text-align:right; font-size:8px; margin-right:20px;"><span class="pageNumber"></span> / <span class="totalPages"></span></div>`,
-                        width: '210mm',
-                        height: '297mm',
-                        margin: {
-                            top: "5px",
-                            bottom: "20px", // margem ajustada para acomodar o rodapé
-                            left: "20px",
-                            right: "20px"
+        });
 
-                        },
-                    });
-                    await browser.close();
-                    return Buffer.from(pdfBuffer);
-                })
-
-
-        } catch (error) {
-            reject(error);
-        }
-
-    })
+        await browser.close();
+        return Buffer.from(pdfBuffer);
+    } catch (error) {
+        throw error;
+    }
 }
