c206223773d176009aa51ebd0f3032cbf8af8222
Sabalo Zua <sabalozua@outlook.pt>
2025-06-09 20:37:20 +0000

    fix: correct email variable reference in sendEmailVerify function

--- Modules/SendEmailVerify.ts
+++ Modules/SendEmailVerify.ts
@@ -12,22 +12,21 @@ import nodemailer from 'nodemailer'
     },
   })
 
-
-  export const sendeemailverfy= async (email: string,  url: string) => { 
-    try{
-	await transporter.sendMail({
-		from: process.env.USER_EMAIL,
-		to: email,
-		subject: "Verifcação de EMail",
+ export const sendeemailverfy= async (email: string,  url: string) => { 
+  try{
+ await  transporter.sendMail({  
+  from: process.env.EMAIL,
+  to: email,
+ 		subject: "Verifcação de EMail",
 		html: `<div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f5f5f5; border-radius: 10px; text-align: center;">
                 <h1 style="color:  #1674E3;">Confirme seu email</h1>
                 <p style="margin-bottom: 20px;">Por favor, clique no link abaixo para verificar seu email no BPA NET:</p>
                 <a href=${url} style="display: inline-block; padding: 10px 20px; background-color:  #1674E3; color: #ffffff; text-decoration: none; border-radius: 5px;">Verificar Email</a>
                   <p style="margin-top: 20px;">Se você não tentou realizar esta operação, entre em contacto com nosso suporte em <a href="https://www.bpa.ao/particulares/apoio-ao-cliente/" style="color: #1674E3; text-decoration: underline;">Linha de Atendimento do BPA</a>. Por favor, não compartilhe este email com ninguém.</p>
             </div>`,
-	});
+      
+ })
 }catch(err){
-  throw new Error(typeof err === 'string' ? err : 'An unknown error occurred')  
+  throw new Error(typeof err === 'string' ? err : 'An unknown error occurred')
 }
-
-};
+}
