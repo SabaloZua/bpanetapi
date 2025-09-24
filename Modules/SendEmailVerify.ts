2f252b5b2fc276595c7184ba17b046d3bc29797d
Sabalo Zua <sabalozua@outlook.pt>
2025-09-24 17:32:44 +0000

    fix: update email verification header for clarity

--- Modules/SendEmailVerify.ts
+++ Modules/SendEmailVerify.ts
@@ -16,10 +16,10 @@ import nodemailer from 'nodemailer'
   to: email,
  		subject: "Verificação BPA NET",
 		html: `<div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f5f5f5; border-radius: 10px; text-align: center;">
-                <h1 style="color:  #1674E3;">Confirme seu email</h1>
+                <h1 style="color:  #1674E3;">Confirme</h1>
                 <p style="margin-bottom: 20px;">Por favor,clique no link abaixo para continuar o seu processo de abertura de conta no BPA NET:</p>
-                <a href=${url} style="display: inline-block; padding: 10px 20px; background-color:  #1674E3; color: #ffffff; text-decoration: none; border-radius: 5px;">Verificar Email</a>
-                  <p style="margin-top: 20px;">Se você não tentou realizar esta operação, entre em contacto com nosso suporte em <a href="https://www.bpa.ao/particulares/apoio-ao-cliente/" style="color: #1674E3; text-decoration: underline;">Linha de Atendimento do BPA</a>. Por favor, não compartilhe este email com ninguém.</p>
+                <a href="${url}" style="display: inline-block; padding: 10px 20px; background-color:  #1674E3; color: #ffffff; text-decoration: none; border-radius: 5px;">Verificar Email</a>
+                  <p style="margin-top: 20px;">Se você não tentou realizar esta operação, entre em contacto com nosso suporte em <a href="https://bpa-net.vercel.app" style="color: #1674E3; text-decoration: underline;">Linha de Atendimento do BPA</a>. Por favor, não compartilhe este email com ninguém.</p>
             </div>`,
       
  })
