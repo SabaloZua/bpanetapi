5a79692f30e436237c4712770445149d16805d1c
Sabalo Zua <sabalozua@outlook.pt>
2025-09-28 22:43:39 +0000

    fix: correct email subject in 2FA and verification email sending functions

--- Modules/Send2fa.ts
+++ Modules/Send2fa.ts
@@ -5,7 +5,7 @@ import { sendEmail } from '../Modules/SendEmail'
   export const sendcodigo2fa=async (email: string, OTP: string)=> {
   try{
 
-    await sendEmail(email, "Código 2FA BPA NET", 2, {OTP: OTP})
+    await sendEmail(email, "Código 2FA BPA NET", 2, {OTP:OTP})
 }catch(err){
   throw new Error(typeof err === 'string' ? err : 'An unknown error occurred')
 }
--- Modules/SendEmailVerify.ts
+++ Modules/SendEmailVerify.ts
@@ -4,7 +4,7 @@ import { sendEmail } from '../Modules/SendEmail'
  export const sendeemailverfy= async (email: string,  url: string) => { 
   try{
 
-    await sendEmail(email, "Verificação BPA NET", 1, {verification_url: url})
+    await sendEmail(email, "Cadastro no BPA NET", 1, {verification_url: url})
      
 }catch(err){
   throw new Error(typeof err === 'string' ? err : 'An unknown error occurred'+err);
